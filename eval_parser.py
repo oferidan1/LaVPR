cf557693184d34693d08fc461584549258e70c77
Ofer Idan <70523006+oferidan1@users.noreply.github.com>
2026-06-21 19:59:37 +0300

    license

--- eval_parser.py
+++ eval_parser.py
@@ -12,8 +12,8 @@ def parse_arguments():
     parser.add_argument("--database_folder", type=str, default="/mnt/d/data/amstertime/test/database")    
     parser.add_argument("--queries_folder", type=str, default="/mnt/d/data/amstertime/test/queries")        
     parser.add_argument("--image_root", type=str, default="/mnt/d/data/amstertime/test")
-    parser.add_argument("--queries_csv", type=str, default="datasets/descriptions/amstertime_descriptions.csv")
-    #parser.add_argument("--queries_csv", type=str, default="datasets/descriptions/amstertime_descriptions_subset.csv")            
+    #parser.add_argument("--queries_csv", type=str, default="datasets/descriptions/amstertime_descriptions.csv")
+    parser.add_argument("--queries_csv", type=str, default="datasets/descriptions/amstertime_descriptions_subset.csv")            
     
     # parser.add_argument("--database_folder", type=str, default="/mnt/d/data/pitts30k/images/test/database")    
     # parser.add_argument("--queries_folder", type=str, default="/mnt/d/data/pitts30k/images/test/queries")    
